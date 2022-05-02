9541d8b703956d1442f94985dc9c157c227f111a
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-05-02 15:23:29 -0300

    oh god

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Main from './layout/main'
-
+import styled, { keyframes } from 'styled-components'
 function App() {
   return (
     <>
-      <Main />
+      <Fade>
+        <Main />
+      </Fade>
     </>
   )
 }
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const Fade = styled.div`
+  animation: 0.7s ${fadeIn} ease-out;
+`
 
 export default App
