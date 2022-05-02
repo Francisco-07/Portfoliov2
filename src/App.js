7aa68474f922df7ed1423eda7e1989ef6c23f969
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-05-02 10:41:54 -0300

    ahora si creo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Main from './layout/main'
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return <Main />
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+  }, [])
+  return loading ? <Main /> : null
 }
 
 export default App
