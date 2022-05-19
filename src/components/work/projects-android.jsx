ae7e2ddd6043e71b5ec5d80a4aa462abc69dffc8
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-05-19 14:09:08 -0300

    retokes

--- src/components/work/projects-android.jsx
+++ src/components/work/projects-android.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { colors, device } from '../../utils'
+import { device } from '../../utils'
 import { useInView } from 'react-intersection-observer'
 
 // ICONOS
