a6b5e639410ebbb5776bb789b67de5d34c3b0ec4
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-09-20 19:16:23 -0300

    no loading

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,37 +3,29 @@ import AboutContainer from '../containers/about/AboutContainer'
 import ProjectsContainer from '../containers/projects/ProjectsContainer'
 import FooterContainer from '../containers/footer/FooterContainer'
 import Header from '../containers/header/Header'
-import Loading from '../components/loading/Loading'
-import { useState, useEffect } from 'react'
+
 import { useInView } from 'react-intersection-observer'
 import styled from 'styled-components'
 
 function Main() {
-  const [loading, setLoading] = useState(false)
   const [ref, inView] = useInView({ triggerOnce: true })
-  useEffect(() => {
-    setTimeout(() => setLoading(true), 1200)
-  }, [])
+
   return (
     <>
-      {loading ? (
-        <Container ref={ref} inView={inView}>
-          <Header />
-          <RedesDesktop />
-          <AboutContainer />
-          <ProjectsContainer />
-          <FooterContainer />
-        </Container>
-      ) : (
-        <Loading />
-      )}
+      <Container ref={ref} inView={inView}>
+        <Header />
+        <RedesDesktop />
+        <AboutContainer />
+        <ProjectsContainer />
+        <FooterContainer />
+      </Container>
     </>
   )
 }
 
 const Container = styled.div`
   opacity: ${({ inView }) => (inView ? '1' : '0')};
-  transition: all 2s;
+  transition: all 2.5s;
 `
 
 export default Main
